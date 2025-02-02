0c18b0da62f87a1fcf8cfa2a36facf3b77462770
kapton13 <antonkapusta4@gmail.com>
2025-02-02 17:28:18 +0200

    added row and column numbering

--- src/components/Matrix/Matrix.tsx
+++ src/components/Matrix/Matrix.tsx
@@ -9,8 +9,19 @@ const Matrix: React.FC = () => {
     <div className={styles.matrixContainer}>
       {matrix.length > 0 && (
         <div className={styles.matrix}>
+          <div className={styles.row}>
+            <div className={`${styles.headerCell} ${styles.cornerCell}`}>#</div>
+            {matrix[0].map((_, colIndex) => (
+              <div key={`col-${colIndex}`} className={styles.headerCell}>
+                {colIndex + 1}
+              </div>
+            ))}
+          </div>
+
           {matrix.map((row, rowIndex) => (
             <div key={rowIndex} className={styles.row}>
+              <div className={styles.headerCell}>{rowIndex + 1}</div>
+              
               {row.map((cell) => (
                 <div key={cell.id} className={styles.cell}>
                   <div className={styles.cellId}>{cell.id}</div>
