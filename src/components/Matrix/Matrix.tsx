92dd307ca12d850625c0d2e0044f796dcb144ca3
kapton13 <antonkapusta4@gmail.com>
2025-02-02 18:17:42 +0200

    added row 50th percentile

--- src/components/Matrix/Matrix.tsx
+++ src/components/Matrix/Matrix.tsx
@@ -5,42 +5,54 @@ import styles from './Matrix.module.css';
 const Matrix: React.FC = () => {
   const { matrix } = useMatrixContext();
 
+  if (matrix.length === 0) return null;
+
+  const columnSums = matrix[0].map((_, colIndex) => 
+    matrix.reduce((sum, row) => sum + row[colIndex].amount, 0)
+  );
+
+  const percentiles = columnSums.map((sum) => Math.floor(sum / 2));
+
   return (
     <div className={styles.matrixContainer}>
-      {matrix.length > 0 && (
-        <div className={styles.matrix}>
-          <div className={styles.row}>
-            <div className={`${styles.headerCell} ${styles.cornerCell}`}>#</div>
-            {matrix[0].map((_, colIndex) => (
-              <div key={`col-${colIndex}`} className={styles.headerCell}>
-                {colIndex + 1}
-              </div>
-            ))}
-            <div className={styles.headerCell}>Sum values</div>
-          </div>
-
-          {matrix.map((row, rowIndex) => {
-            const rowSum = row.reduce((sum, cell) => sum + cell.amount, 0);
-
-            return (
-              <div key={rowIndex} className={styles.row}>
-                <div className={styles.headerCell}>{rowIndex + 1}</div>
-
-                {row.map((cell) => (
-                  <div key={cell.id} className={styles.cell}>
-                    <div className={styles.cellId}>{cell.id}</div>
-                    <div className={styles.amount}>{cell.amount}</div>
-                  </div>
-                ))}
-
-                <div className={`${styles.cell} ${styles.sumCell}`}>
-                  {rowSum}
-                </div>
-              </div>
-            );
-          })}
+      <div className={styles.matrix}>
+        <div className={styles.row}>
+          <div className={`${styles.headerCell} ${styles.cornerCell}`}>#</div>
+          {matrix[0].map((_, colIndex) => (
+            <div key={`col-${colIndex}`} className={styles.headerCell}>
+              {colIndex + 1}
+            </div>
+          ))}
+          <div className={styles.headerCell}>Sum values</div>
         </div>
-      )}
+
+        {matrix.map((row, rowIndex) => {
+          const rowSum = row.reduce((sum, cell) => sum + cell.amount, 0);
+
+          return (
+            <div key={rowIndex} className={styles.row}>
+              <div className={styles.headerCell}>{rowIndex + 1}</div>
+              {row.map((cell, colIndex) => (
+                <div key={cell.id} className={`${styles.cell} ${styles.equalCell}`} style={{ width: `${100 / (matrix[0].length + 1)}%` }}>
+                  <div className={styles.cellId}>{cell.id}</div>
+                  <div className={styles.amount}>{cell.amount}</div>
+                </div>
+              ))}
+              <div className={`${styles.cell} ${styles.sumCell} ${styles.equalCell}`}>{rowSum}</div>
+            </div>
+          );
+        })}
+
+        <div className={styles.row}>
+          <div className={`${styles.headerCell} ${styles.cornerCell}`}>50th percentile</div>
+          {percentiles.map((percentile, colIndex) => (
+            <div key={`percentile-${colIndex}`} className={`${styles.cell} ${styles.footerRow} ${styles.equalCell}`}>
+              {percentile}
+            </div>
+          ))}
+          <div className={styles.headerCell}></div>
+        </div>
+      </div>
     </div>
   );
 };
